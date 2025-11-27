4f94a07b55d5cffed2660defb3466652fadaaba0
Nathalia Debiasi~ <nathaliaramosdebiasi@gmail.com~>
2025-11-27 10:27:06 -0800

    Feat (BE): Chest | Update createChest method to allow optional items parameter

--- backend/src/modules/content/chest/chest.model.ts
+++ backend/src/modules/content/chest/chest.model.ts
@@ -1,15 +1,15 @@
 import { prisma } from "../../../infrastructure/database/prisma";
 
 export const ChestModel = {
-  async createChest(cabinetId: number, hashedPasscode: string) {
-    return await prisma.chest.create({
-        data: { cabinetId, passcode: hashedPasscode },
-    });
-  },
-
   async getChestByCabinetId(cabinetId: number) {
     return await prisma.chest.findUnique({
         where: { cabinetId },
+    });
+  },
+
+  async createChest(cabinetId: number, hashedPasscode: string, items?: any[]) {
+    return await prisma.chest.create({
+        data: { cabinetId, passcode: hashedPasscode, items: items ? items : [] },
     });
   },
 
