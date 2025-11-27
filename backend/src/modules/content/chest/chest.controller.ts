e795d0730a1d0ff0afff3a9ccd51a396e5463f39
Nathalia Debiasi~ <nathaliaramosdebiasi@gmail.com~>
2025-11-27 10:27:16 -0800

    Feat (BE): Chest | Refactor ChestController to improve error handling and add upload signature method

--- backend/src/modules/content/chest/chest.controller.ts
+++ backend/src/modules/content/chest/chest.controller.ts
@@ -1,33 +1,65 @@
 import { ChestModel } from "./chest.model";
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
+import { generateUploadSignature } from "../../../lib/cloudinary";
+import type { CloudinarySignature } from "../../../types/cloudinary.types";
+import { CloudinaryFolders } from "../../../lib/cloudinaryFolders";
 
 export const ChestController = {
-  createChest: async (req: Request, res: Response) => {
-    const cabinetId = Number(req.params.cabinetId);
-    
-    const { passcode } = req.body;
-    if (!passcode) {
-      return res.status(400).json({ message: "Passcode is required" });
+  getUploadSignature: async (req: Request, res: Response) => {
+    try {
+      const folder = req.query.folder as string;
+      const upload_preset = req.query.upload_preset as string;
+
+      const payload: CloudinarySignature = generateUploadSignature(CloudinaryFolders.CHEST, upload_preset);
+
+      res.json(payload);
+    } catch (err) {
+      console.error("GET upload signature error:", err);
+      res.status(500).json({ error: "Server error" });
     }
-    const hashedPasscode = await bcrypt.hash(passcode, 10);
-    const newChest = await ChestModel.createChest(Number(cabinetId), hashedPasscode);
-    return res.json(newChest);
   },
 
-    getChest: async (req: Request, res: Response) => {
-        const { cabinetId } = req.params;
-        const chest = await ChestModel.getChestByCabinetId(Number(cabinetId));
-        return res.json(chest);
-    },
+  getChest: async (req: Request, res: Response) => {
+    try {    
+      const { cabinetId } = req.params;
+      const chest = await ChestModel.getChestByCabinetId(Number(cabinetId));
+      res.json(chest);
+    } catch (err) {
+      console.error("GET chest error:", err);
+      res.status(500).json({ error: "Server error" });
+    }
+  },
 
-    openChest: async (req: Request, res: Response) => {
+  createChest: async (req: Request, res: Response) => {
+    try {
+     const cabinetId = Number(req.params.cabinetId);
+    
+     const { passcode } = req.body;
+      if (!passcode) {
+        return res.status(400).json({ message: "Passcode is required" });
+      }
+      const hashedPasscode = await bcrypt.hash(passcode, 10);
+
+      const newChest = await ChestModel.createChest(
+        cabinetId, 
+        hashedPasscode,
+        req.body.items,
+      );
+      res.json(newChest);
+    } catch (err) {
+      console.error("CREATE chest error:", err);
+      res.status(500).json({ error: "Server error" });
+    }
+  },
+
+  openChest: async (req: Request, res: Response) => {
     try {
         const chest = (req as any).chest;
 
         res.json({
             id: chest.id,
-            contents: chest.contents,
+            items: chest.items,
         });
     } catch (err) {
       console.error("OPEN journal error:", err);
@@ -36,11 +68,19 @@ export const ChestController = {
   },
 
     addChestContent: async (req: Request, res: Response) => {
-        const { cabinetId } = req.params;
-        const { items } = req.body;
-        const chest = await ChestModel.getChestByCabinetId(Number(cabinetId));
-       
-        const updatedChest = await ChestModel.addChestContent(chest!.id, items);
-        return res.json(updatedChest);
+      try {
+          const { cabinetId } = req.params;
+          const { items } = req.body;
+          const chest = await ChestModel.getChestByCabinetId(Number(cabinetId));
+          const updatedChest = await ChestModel.addChestContent(
+            chest!.id, 
+            items
+          );
+          res.json(updatedChest);
+        } 
+        catch (err) {
+            console.error("ADD chest content error:", err);
+            res.status(500).json({ error: "Server error" });
+       }
     },
 };
