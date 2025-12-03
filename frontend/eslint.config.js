006161a6f693c8cba485b4d2ecc24159310ef1fb
Nathalia Debiasi~ <nathaliaramosdebiasi@gmail.com~>
2025-12-02 16:15:33 -0800

    Chore (FE): Setup | Update package.json and define global font types

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,25 +1,32 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
-export default [
-  {
-    files: ["**/*.{ts,tsx,js,jsx}"],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module"
-      },
-      globals: {
-        ...globals.browser,
-        process: "readonly"
-      }
+export default {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    plugins: { "@typescript-eslint": tseslint.plugin },
-    rules: {
-      ...pluginJs.configs.recommended.rules,
-      ...tseslint.configs.recommended.rules,
-    }
-  }
-];
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'import', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended'
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // optional custom rules
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'react/react-in-jsx-scope': 'off', // React 18 no longer needs React in scope
+  },
+};
